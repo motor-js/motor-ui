ba133b74250ab985199b2dc28a322882ea697519
lureilly1 <lureilly1@gmail.com>
2020-06-26 14:05:11 +0100

    starter comit

---
+++ src/lib/components/React/Button/index.d.ts
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+export interface ButtonProps {
+}
+
+declare const Button: React.FC<ButtonProps & Omit<JSX.IntrinsicElements['button'], 'color'>>;
+export type ButtonType = ButtonProps & Omit<JSX.IntrinsicElements['button'], 'color'>
+
+export { Button };
