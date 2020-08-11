9435a8e99ce1275064472c5d661555d54b6b98f3
lureilly1 <lureilly1@gmail.com>
2020-08-11 09:48:06 +0100

    Login Form

--- src/docs/config/config.js
+++ src/docs/config/config.js
@@ -1,5 +1,17 @@
 
 export const config = {
+  host: 'juno-ui.eu.qlikcloud.com',
+  secure: true,
+  port: null,
+  prefix: '',
+  appId: 'f29964d4-0e59-48a2-8525-34dab9cd41e7',
+  qcs: true,
+  webIntId: '4Tx-ydWxSQEM_q1ajlYBVzGgVUVJUo-i',
+}
+
+/*
+
+export const config = {
   host: 'sense-demo.qlik.com',
   secure: true,
   port: 443,
@@ -7,7 +19,7 @@ export const config = {
   appId:'372cbc85-f7fb-4db6-a620-9a5367845dce',
 }
 
-/*
+
 export const config = {
   host: 'juno-ui.eu.qlikcloud.com',
   secure: true,
