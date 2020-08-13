a2fcbf2df042be2ed17b242636572186e226a717
Alan Green <Alan_Green@Alans-MacBook-Pro.local>
2020-08-13 08:19:01 +0100

    Vertical bar data-ui complete

--- src/lib/components/React/data-ui-test/VerticalBarChart.jsx
+++ src/lib/components/React/data-ui-test/VerticalBarChart.jsx
@@ -1,43 +1,14 @@
 /* eslint react/prop-types: 0 */
 import React from "react";
-import { timeParse, timeFormat } from "d3-time-format";
-import {
-  CrossHair,
-  XAxis,
-  YAxis,
-  BarSeries,
-  PatternLines,
-  LinearGradient,
-} from "../../VX/xy-chart";
+import { BarSeries, PatternLines, LinearGradient } from "../../VX/xy-chart";
 import WithTooltip from "../../VX/old/composer/WithTooltip";
-import { color as colors, allColors } from "../../VX/theme";
-import { xTickStyles, yTickStyles } from "../../VX/theme/chartTheme";
+import { color } from "../../VX/theme";
 import ResponsiveXYChart from "./ResponsiveXYChart";
 
 import { timeSeriesData } from "./data";
 
-const categoryHorizontalData = timeSeriesData.map((d, i) => ({
-  x: d.y,
-  y: i + 1,
-  selected: false,
-  label: i === 3 ? "Long long label" : (i === 5 && "Label") || "",
-}));
-
-class HorizontalBarChartExample extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      direction: "vertical",
-      data: categoryHorizontalData,
-    };
-  }
-
+class VaerticalBarChartExample extends React.PureComponent {
   render() {
-    const { direction, data } = this.state;
-    const categoryScale = { type: "band", paddingInner: 0.1 };
-    const valueScale = { type: "linear" };
-    const horizontal = direction === "horizontal";
-
     return (
       <div className="vertical-bar-demo">
         <WithTooltip
@@ -60,14 +31,14 @@ class HorizontalBarChartExample extends React.PureComponent {
           >
             <LinearGradient
               id="gradient"
-              from={colors.default}
-              to={colors.dark}
+              from={color.default}
+              to={color.dark}
             />
             <PatternLines
               id="lines"
               height={8}
               width={8}
-              stroke={colors.categories[2]}
+              stroke={color.categories[2]}
               background="#fff"
               strokeWidth={1}
               orientation={["horizontal", "vertical"]}
@@ -87,4 +58,4 @@ class HorizontalBarChartExample extends React.PureComponent {
   }
 }
 
-export default HorizontalBarChartExample;
+export default VaerticalBarChartExample;
