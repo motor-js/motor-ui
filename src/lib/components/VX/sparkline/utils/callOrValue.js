3ede68db7e9c0145dda2078f809585bd21c27aa9
Alan Green <Alan_Green@Alans-MacBook-Pro.local>
2020-08-28 14:54:36 +0100

    Remocval of sparkline prep work

--- src/lib/components/VX/sparkline/utils/callOrValue.js
+++
@@ -1,7 +0,0 @@
-export default function callOrValue(maybeFn, ...args) {
-  if (typeof maybeFn === 'function') {
-    return maybeFn(...args);
-  }
-
-  return maybeFn;
-}
--- src/lib/components/VX/sparkline/utils/defined.js
+++
@@ -1,4 +0,0 @@
-// eslint-disable-next-line no-restricted-globals
-const defined = d => typeof d !== 'undefined' && d !== null && !isNaN(d);
-
-export default defined;
