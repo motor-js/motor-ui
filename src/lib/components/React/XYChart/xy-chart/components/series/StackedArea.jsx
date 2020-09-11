849f439260abedde028e8882c215c4b7277d1d98
Alan Green <Alan_Green@Alans-MacBook-Pro.local>
2020-09-11 09:01:58 +0100

    StackedArea bugfix

--- src/lib/components/React/XYChart/xy-chart/components/series/StackedArea.jsx
+++ src/lib/components/React/XYChart/xy-chart/components/series/StackedArea.jsx
@@ -8,12 +8,11 @@ import React, {
 import { extent } from "d3-array";
 import AreaStack from "./AreaStack";
 import ChartContext from "../../context/ChartContext";
-// import interpolatorLookup from "../../util/interpolatorLookup";
 
 import findNearestDatumY from "../../util/findNearestDatumY";
 import findNearestDatumX from "../../util/findNearestDatumX";
 
-export default function Stack({ children, ...rectProps }) {
+export default function Stack({ horizontal, children, ...rectProps }) {
   const {
     xScale,
     yScale,
@@ -24,12 +23,10 @@ export default function Stack({ children, ...rectProps }) {
     height,
     margin,
     theme,
-    handleClick,
-    currentSeelctionIds,
+    // handleClick,
+    // currentSeelctionIds,
     // selectionIds,
   } = useContext(ChartContext) || {};
-
-  const interpolation = "monotoneX";
 
   // extract data keys from child series
   const dataKeys = useMemo(
@@ -39,6 +36,56 @@ export default function Stack({ children, ...rectProps }) {
 
   // use a ref to the stacks for mouse movements
   const stacks = useRef(null);
+
+  // override the findNearestDatum logic
+  const findNearestDatum = useCallback(
+    (args) => {
+      if (!stacks.current) return null;
+
+      const nearestDatum = horizontal
+        ? findNearestDatumY(args)
+        : findNearestDatumX(args);
+
+      if (!nearestDatum) return null;
+
+      // find the stack for this key, and the bar in that stack corresponding to nearestDatum
+      const stack = stacks.current.find(
+        (currStack) => currStack.key === args.key
+      );
+      const bar = stack?.bars?.[nearestDatum.index];
+
+      if (!bar) return null;
+
+      const distanceX = horizontal
+        ? // if svgMouseX is *on* the bar, set distance to 0
+          args.svgMouseX >= bar.x && args.svgMouseX <= bar.x + bar.width
+          ? 0
+          : // otherwise take the min distance between the left and the right of the bar
+            Math.min(
+              Math.abs(args.svgMouseX - bar.x),
+              Math.abs(args.svgMouseX - (bar.x + bar.width))
+            )
+        : nearestDatum.distanceX;
+
+      const distanceY = horizontal
+        ? nearestDatum.distanceY
+        : // if svgMouseY is *on* the bar, set distance to 0
+        args.svgMouseY >= bar.y && args.svgMouseY <= bar.y + bar.height
+        ? 0
+        : // otherwise take the min distance between the top and the bottom of the bar
+          Math.min(
+            Math.abs(args.svgMouseY - bar.y),
+            Math.abs(args.svgMouseY - (bar.y + bar.height))
+          );
+
+      return {
+        ...nearestDatum,
+        distanceX,
+        distanceY,
+      };
+    },
+    [horizontal]
+  );
 
   // group all child data by stack value, this format is needed by BarStack
   const combinedData = useMemo(() => {
@@ -56,7 +103,7 @@ export default function Stack({ children, ...rectProps }) {
       if (!xAccessor || !yAccessor || !elAccessor) return;
 
       data.forEach((d) => {
-        const stack = xAccessor(d);
+        const stack = (horizontal ? yAccessor : xAccessor)(d);
         const stackKey = String(stack);
         if (!dataByStackValue[stackKey]) {
           dataByStackValue[stackKey] = {
@@ -65,7 +112,7 @@ export default function Stack({ children, ...rectProps }) {
             negativeSum: 0,
           };
         }
-        const value = yAccessor(d);
+        const value = (horizontal ? xAccessor : yAccessor)(d);
         dataByStackValue[stackKey][dataKey] = value;
         dataByStackValue[stackKey]["selectionId"] = elAccessor(d);
         dataByStackValue[stackKey][
@@ -75,7 +122,19 @@ export default function Stack({ children, ...rectProps }) {
     });
 
     return Object.values(dataByStackValue);
-  }, [children]);
+  }, [horizontal, children]);
+
+  // update the domain to account for the (directional) stacked value
+  const comprehensiveDomain = useMemo(
+    () =>
+      extent(
+        combinedData
+          .map((d) => d.positiveSum)
+          .concat(combinedData.map((d) => d.negativeSum)),
+        (d) => d
+      ).filter((val) => val != null),
+    [combinedData]
+  );
 
   // register all child data
   useEffect(() => {
@@ -97,14 +156,38 @@ export default function Stack({ children, ...rectProps }) {
         yAccessor,
         elAccessor,
         mouseEvents,
+        findNearestDatum,
       };
+
+      // only need to update the domain for one of the keys
+      if (comprehensiveDomain.length > 0 && dataKeys.indexOf(key) === 0) {
+        if (horizontal) {
+          dataToRegister[key].xScale = (scale) =>
+            scale.domain(
+              extent([...scale.domain(), ...comprehensiveDomain], (d) => d)
+            );
+        } else {
+          dataToRegister[key].yScale = (scale) =>
+            scale.domain(
+              extent([...scale.domain(), ...comprehensiveDomain], (d) => d)
+            );
+        }
+      }
     });
 
     registerData(dataToRegister);
 
     // unregister data on unmount
     return () => unregisterData(Object.keys(dataToRegister));
-  }, [registerData, unregisterData, children, dataKeys]);
+  }, [
+    horizontal,
+    comprehensiveDomain,
+    registerData,
+    unregisterData,
+    children,
+    findNearestDatum,
+    dataKeys,
+  ]);
 
   // if scales and data are not available in the registry, bail
   if (
@@ -116,11 +199,7 @@ export default function Stack({ children, ...rectProps }) {
     return null;
   }
 
-  // console.log(combinedData);
-
-  // const getY0 = (d) => d && d.y0;
-  // const getY1 = (d) => d && d.y1;
-  // const x = (d) => d && d.x;
+  const hasSomeNegativeValues = comprehensiveDomain.some((num) => num < 0);
 
   return (
     // @TODO types
@@ -132,6 +211,7 @@ export default function Stack({ children, ...rectProps }) {
       x={(d) => xScale(d.data.stack)}
       y0={(d) => yScale(d[0])}
       y1={(d) => yScale(d[1])}
+      offset={hasSomeNegativeValues ? "diverging" : undefined}
       color={colorScale}
     >
       {({ stacks, path, color }) =>
