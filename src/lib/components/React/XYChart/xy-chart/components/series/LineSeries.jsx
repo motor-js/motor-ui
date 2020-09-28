5f5d9d3d6f0d35a13e718b6f72bf8456c9b63174
Alan Green <Alan_Green@Alans-MacBook-Pro.local>
2020-09-28 19:08:10 +0100

    lables added to LineSeries

--- src/lib/components/React/XYChart/xy-chart/components/series/LineSeries.jsx
+++ src/lib/components/React/XYChart/xy-chart/components/series/LineSeries.jsx
@@ -1,6 +1,7 @@
-import React, { useContext, useCallback, useState, useEffect } from "react";
+import React, { useContext, useCallback } from "react";
 import { animated, useSpring } from "react-spring";
 import { LinePath } from "@visx/shape";
+import { Text } from "@visx/text";
 import ChartContext from "../../context/ChartContext";
 import TooltipContext from "../../context/TooltipContext";
 import withRegisteredData from "../../enhancers/withRegisteredData";
@@ -84,6 +85,8 @@ function LineSeries({
     [findNearestData, showTooltip]
   );
 
+  const getValue = (d) => d.filter((val) => val.qText === dataKey)[0].qNum;
+
   return (
     <g className="visx-group line-series">
       <LinePath data={data} x={getScaledX} y={getScaledY} {...lineProps}>
@@ -92,50 +95,62 @@ function LineSeries({
         )}
       </LinePath>
 
-      {showPoints &&
-        data.map((d, i) => {
-          const left = getScaledX(d);
-          const top = getScaledY(d);
-          d.selectionId = getElemNumber(d);
-          return (
-            <g key={`line-glyph-${i}`}>
-              <ChartGlyph
-                left={left}
-                top={top}
-                size={
-                  isDefined(glyph)
-                    ? glyph.size
-                    : showPoints.size || theme.points.size
-                }
-                fill={isDefined(glyph) ? glyph.fill : color}
-                stroke={isDefined(glyph) ? glyph.stroke : color}
-                strokeWidth={
-                  isDefined(glyph)
-                    ? glyph.strokeWidth
-                    : showPoints.strokeWidth || theme.points.strokeWidth
-                }
-                style={{ cursor: "pointer " }}
-                onClick={() => {
-                  const selections = currentSelectionIds.includes(d.selectionId)
-                    ? currentSelectionIds.filter(function(value) {
-                        return value !== d.selectionId;
-                      })
-                    : [...currentSelectionIds, d.selectionId];
-                  handleClick(selections);
-                }}
-                onMouseMove={onMouseMove}
-                onMouseLeave={() => {
-                  hideTooltip();
-                }}
-              />
-              {/* {showLabels && (
-                <Text {...labelProps} key={`line-label-${i}`} x={left} y={top}>
-                  {formatValue(d[1].qNum)}
-                </Text>
-              )} */}
-            </g>
-          );
-        })}
+      {showPoints ||
+        (showLabels &&
+          data.map((d, i) => {
+            const left = getScaledX(d);
+            const top = getScaledY(d);
+            d.selectionId = getElemNumber(d);
+            return (
+              <g key={`line-glyph-${i}`}>
+                {showPoints && (
+                  <ChartGlyph
+                    left={left}
+                    top={top}
+                    size={
+                      isDefined(glyph)
+                        ? glyph.size
+                        : showPoints.size || theme.points.size
+                    }
+                    fill={isDefined(glyph) ? glyph.fill : color}
+                    stroke={isDefined(glyph) ? glyph.stroke : color}
+                    strokeWidth={
+                      isDefined(glyph)
+                        ? glyph.strokeWidth
+                        : showPoints.strokeWidth || theme.points.strokeWidth
+                    }
+                    style={{ cursor: "pointer " }}
+                    onClick={() => {
+                      const selections = currentSelectionIds.includes(
+                        d.selectionId
+                      )
+                        ? currentSelectionIds.filter(function(value) {
+                            return value !== d.selectionId;
+                          })
+                        : [...currentSelectionIds, d.selectionId];
+                      handleClick(selections);
+                    }}
+                    onMouseMove={onMouseMove}
+                    onMouseLeave={() => {
+                      hideTooltip();
+                    }}
+                  />
+                )}
+                {showLabels && (
+                  <Text
+                    {...labelProps}
+                    key={`line-label-${i}`}
+                    x={left}
+                    y={top}
+                    dx={horizontal ? "0.5em" : 0}
+                    dy={horizontal ? 0 : "-0.74em"}
+                  >
+                    {formatValue(getValue(d))}
+                  </Text>
+                )}
+              </g>
+            );
+          }))}
     </g>
   );
 }
