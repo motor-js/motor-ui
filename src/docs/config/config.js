87a4880d8456ee7bdc216b0d6764fa2bf35afc3d
lureilly1 <lureilly1@gmail.com>
2020-11-07 16:09:23 +0000

    Docz re-build

---
+++ src/docs/config/config.js
@@ -0,0 +1,49 @@
+export const config = {
+  host: 'juno-ui.eu.qlikcloud.com',
+  secure: true,
+  port: null,
+  prefix: '',
+  appId: 'f29964d4-0e59-48a2-8525-34dab9cd41e7',
+  qcs: true,
+  webIntId: '4Tx-ydWxSQEM_q1ajlYBVzGgVUVJUo-i',
+}
+
+/*
+export const config = {
+  host: "localhost",
+  secure: false,
+  port: 19077,
+  prefix: "",
+  appId: "fd74489e-81c4-47ae-93b3-06ad49175b4e",
+  // appId: "ac09dd54-6bc5-45fe-bb95-ad6f329737be",
+};
+
+/*
+
+export const config = {
+  host: 'sense-demo.qlik.com',
+  secure: true,
+  port: 443,
+  prefix: '',
+  appId:'372cbc85-f7fb-4db6-a620-9a5367845dce',
+}
+
+*/
+
+/*
+export const config = {
+  host: "localhost",
+  secure: false,
+  port: 19077,
+  prefix: "",
+  appId: "a21862bc-0e9a-4a16-938c-fe3d03209f4a",
+};
+*/
+
+// export const config = {
+//   host: 'localhost',
+//   secure: false,
+//   port: 19077,
+//   prefix: '',
+//   appId: '02e868df-bbb3-4733-9786-b69edccf4218',
+// };
