b5acd4fe00125e3270361a9e84dc3765550f9cf1
alangrn <alangrn@hotmail.com>
2021-02-16 17:55:10 +0000

    New axis file

--- src/lib/components/visx/components/axis/BaseAxis.tsx
+++ src/lib/components/visx/components/axis/BaseAxis.tsx
@@ -1,15 +1,15 @@
-import React, { useMemo, useContext } from 'react';
-import { AxisScale } from '@visx/axis';
-import { AxisProps as VxAxisProps } from '@visx/axis/lib/axis/Axis';
-import { ScaleInput } from '@visx/scale';
-import DataContext from '../../context/DataContext';
+import React, { useMemo, useContext } from "react";
+import { AxisScale, TickLabelProps } from "@visx/axis";
+import { AxisProps as VxAxisProps } from "@visx/axis/lib/axis/Axis";
+import { ScaleInput } from "@visx/scale";
+import DataContext from "../../context/DataContext";
 
 export type BaseAxisProps<Scale extends AxisScale> = Omit<
   VxAxisProps<Scale>,
-  'scale' | 'orientation'
+  "scale" | "orientation"
 > & {
   /** Required axis orientation. */
-  orientation: NonNullable<VxAxisProps<Scale>['orientation']>;
+  orientation: NonNullable<VxAxisProps<Scale>["orientation"]>;
 } & {
   /** Rendered component which is passed VxAxisProps by BaseAxis after processing. */
   AxisComponent: React.FC<VxAxisProps<Scale>>;
@@ -23,50 +23,55 @@ export default function BaseAxis<Scale extends AxisScale>({
   AxisComponent,
   ...props
 }: BaseAxisProps<Scale>) {
-  const { theme, xScale, yScale, margin, width, height } = useContext(DataContext);
+  const { theme, xScale, yScale, margin, width, height } = useContext(
+    DataContext
+  );
   const { orientation } = props;
 
   const axisStyles = useMemo(
     () =>
-      orientation === 'left' || orientation === 'right'
+      orientation === "left" || orientation === "right"
         ? theme?.axisStyles?.y?.[orientation]
         : theme?.axisStyles?.x?.[orientation],
-    [theme, orientation],
+    [theme, orientation]
   );
 
-  const tickLabelProps = useMemo(
+  const tickLabelProps = useMemo<TickLabelProps<ScaleInput<Scale>> | undefined>(
     () =>
       props.tickLabelProps || axisStyles // construct from props + theme if possible
-        ? (value: ScaleInput<Scale>, index: number) =>
+        ? (value, index, values) =>
             // by default, wrap vertical-axis tick labels within the allotted margin space
             // this does not currently account for axis label
             ({
               ...axisStyles?.tickLabel,
               width:
-                orientation === 'left' || orientation === 'right'
+                orientation === "left" || orientation === "right"
                   ? margin?.[orientation]
                   : undefined,
-              ...props.tickLabelProps?.(value, index),
+              ...props.tickLabelProps?.(value, index, values),
             })
         : undefined,
-
-    [props.tickLabelProps, axisStyles, orientation, margin],
+    [props.tickLabelProps, axisStyles, orientation, margin]
   );
 
   const topOffset =
-    orientation === 'bottom'
+    orientation === "bottom"
       ? (height ?? 0) - (margin?.bottom ?? 0)
-      : orientation === 'top'
+      : orientation === "top"
       ? margin?.top ?? 0
       : 0;
   const leftOffset =
-    orientation === 'left'
+    orientation === "left"
       ? margin?.left ?? 0
-      : orientation === 'right'
+      : orientation === "right"
       ? (width ?? 0) - (margin?.right ?? 0)
       : 0;
 
-  return (
+  const scale = (orientation === "left" || orientation === "right"
+    ? yScale
+    : xScale) as Scale | undefined;
+
+  return scale ? (
     <AxisComponent
       top={topOffset}
       left={leftOffset}
@@ -77,7 +82,7 @@ export default function BaseAxis<Scale extends AxisScale>({
       tickStroke={axisStyles?.tickLine?.stroke}
       {...props}
       tickLabelProps={tickLabelProps}
-      scale={(orientation === 'left' || orientation === 'right' ? yScale : xScale) as Scale}
+      scale={scale}
     />
-  );
+  ) : null;
 }
